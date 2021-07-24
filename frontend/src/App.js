cc1b52cd31a3bdc51b15ba23ebfcdd1402685c52
abdulbasit0-UI <habdulbasit099@gmail.com>
2021-07-24 11:20:50 +0200

    bootstrap navbar header footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+    <Header />
+    <main className="py-3">
+      <Container>
       <h1>hello world</h1>
-    </div>
+      </Container>
+    </main>
+
+    <Footer />
+    </>
   );
 }
 
